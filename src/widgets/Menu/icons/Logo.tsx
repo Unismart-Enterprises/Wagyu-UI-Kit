e9318c01d3b33e5f405c94298f7c900e33950ae2
aman_invincix <aman.jain@invincix.com>
2021-06-12 12:12:42 +0530

    Changed logo

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -13,7 +13,7 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
       <image
         width="205"
         height="40"
-        href={isDark ? "/images/egg/LogoTextNewDark.png" : "/images/egg/LogoTextNewWhite.png"}
+        href={isDark ? "#" : "#"}
       />
     </Svg>
   );
